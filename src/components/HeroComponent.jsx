6a6b743d8b41a0b89e428b0eb56b6237acd69106
Syed25794 <syedgulmohammad25794@gmail.com>
2023-02-26 23:14:00 +0530

    text align in hero component

--- src/components/HeroComponent.jsx
+++ src/components/HeroComponent.jsx
@@ -5,8 +5,8 @@ import { IoSearchOutline } from 'react-icons/io5'
 const HeroComponent = () => {
   return (
     <Box bgImage="url('./HeroSectionImage3.jpg')"  bgSize="cover" bgPosition="center" height={["25rem","30rem","40rem"]}  display="flex" flexDir="column" justifyContent="center" alignItems="center" gap={["0.5rem","0.8rem","1rem"]}>
-      <Heading color="white" fontSize={["2xl","3xl","4xl"]} fontFamily="Montserrat">Download High Quality Images by creators</Heading>
-      <Text fontSize="1rem" color="white" >Over 2.4 million+ stock Images by our talented community</Text>
+      <Heading color="white"  textAlign="center" fontSize={["2xl","3xl","4xl"]} fontFamily="Montserrat">Download High Quality Images by creators</Heading>
+      <Text fontSize="1rem" color="white" textAlign="center" >Over 2.4 million+ stock Images by our talented community</Text>
       <InputGroup fontFamily="Monstserrat" w="75%"  h="3rem">
           <InputLeftElement  h="full" pointerEvents="none" children={<IoSearchOutline  color="gray.300" />}/>
           <Input border="1px solid black" borderRadius="0.5rem" h="full" bgColor="white" type="tel" fontSize="lg" placeholder="Search high resolution Images, categories, wallpapers" />
